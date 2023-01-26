add2ee284c6c5cb0c303f80eb1108824f32d18c4
Synonymous64 <prajwalurkude007@gmail.com>
2023-01-26 22:41:21 +0530

    footer and header implemented

--- client/src/components/common/Logo.jsx
+++ client/src/components/common/Logo.jsx
@@ -1,12 +1,14 @@
 import { Typography, useTheme } from '@mui/material';
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Logo = () => {
     const theme = useTheme();
     return (
-        <Typography fontWeight="700" fontSize="1.7rem">
-            BINGE<span style={{color: theme.palette.primary.main}}>FLIX</span>
-        </Typography>
+        <Link to="/" style={{ cursor: 'pointer', textDecoration: 'none', color: 'white' }}>
+            <Typography fontWeight="700" fontSize="1.7rem">
+                BINGE<span style={{ color: theme.palette.primary.main }}>FLIX</span>
+            </Typography>
+        </Link>
     )
 }
 
