844799b04afdab49787e430460b5b5082bde99d0
Synonymous64 <prajwalurkude007@gmail.com>
2023-02-09 00:20:47 +0530

    few bugs fixed and castslide, mediadetails created

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -1,15 +1,17 @@
 import responseHandler from "../handlers/response.handler.js";
-import tmdbApi from '../tmdb/tmdb.api.js';
+import tmdbApi from "../tmdb/tmdb.api.js";
 import userModel from "../models/user.model.js";
 import favoriteModel from "../models/favorite.model.js";
 import reviewModel from "../models/review.model.js";
-import tokenMiddleware from "../middlewares/token.middleware.js";
+import tokenMiddlerware from "../middlewares/token.middleware.js";
 
 const getList = async (req, res) => {
     try {
-        const { page } = req.query
-        const { mediaType, mediaCategory } = req.params
+        const { page } = req.query;
+        const { mediaType, mediaCategory } = req.params;
+
         const response = await tmdbApi.mediaList({ mediaType, mediaCategory, page });
+
         return responseHandler.ok(res, response);
     } catch {
         responseHandler.error(res);
@@ -19,7 +21,9 @@ const getList = async (req, res) => {
 const getGenres = async (req, res) => {
     try {
         const { mediaType } = req.params;
+
         const response = await tmdbApi.mediaGenres({ mediaType });
+
         return responseHandler.ok(res, response);
     } catch {
         responseHandler.error(res);
@@ -30,13 +34,14 @@ const search = async (req, res) => {
     try {
         const { mediaType } = req.params;
         const { query, page } = req.query;
+
         const response = await tmdbApi.mediaSearch({
             query,
             page,
             mediaType: mediaType === "people" ? "person" : mediaType
         });
-        responseHandler.ok(res, response);
 
+        responseHandler.ok(res, response);
     } catch {
         responseHandler.error(res);
     }
@@ -45,25 +50,39 @@ const search = async (req, res) => {
 const getDetail = async (req, res) => {
     try {
         const { mediaType, mediaId } = req.params;
+
         const params = { mediaType, mediaId };
+
         const media = await tmdbApi.mediaDetail(params);
-        media.credits = await tmdbApi.mediaCredits(params)
+
+        media.credits = await tmdbApi.mediaCredits(params);
+
         const videos = await tmdbApi.mediaVideos(params);
+
         media.videos = videos;
+
         const recommend = await tmdbApi.mediaRecommend(params);
+
         media.recommend = recommend.results;
+
         media.images = await tmdbApi.mediaImages(params);
-        const tokenDecoded = tokenMiddleware.tokenDecode(req);
+
+        const tokenDecoded = tokenMiddlerware.tokenDecode(req);
+
         if (tokenDecoded) {
             const user = await userModel.findById(tokenDecoded.data);
+
             if (user) {
                 const isFavorite = await favoriteModel.findOne({ user: user.id, mediaId });
                 media.isFavorite = isFavorite !== null;
             }
         }
+
         media.reviews = await reviewModel.find({ mediaId }).populate("user").sort("-createdAt");
-        responseHandler.ok(res, mediaId);
-    } catch {
+
+        responseHandler.ok(res, media);
+    } catch (e) {
+        console.log(e);
         responseHandler.error(res);
     }
 };
--- server/src/tmdb/tmdb.api.js
+++ server/src/tmdb/tmdb.api.js
@@ -3,35 +3,35 @@ import tmdbEndpoints from "./tmdb.endpoints.js";
 
 const tmdbApi = {
     mediaList: async ({ mediaType, mediaCategory, page }) => await axiosClient.get(
-        tmdbEndpoints.mediaList({ mediaType, mediaCategory, page }),
+        tmdbEndpoints.mediaList({ mediaType, mediaCategory, page })
     ),
-    mediaDetail: async ({ mediaType, page }) => await axiosClient.get(
-        tmdbEndpoints.mediaDetail({ mediaType, page }),
+    mediaDetail: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaDetail({ mediaType, mediaId })
     ),
     mediaGenres: async ({ mediaType }) => await axiosClient.get(
-        tmdbEndpoints.mediaGenres({ mediaType }),
+        tmdbEndpoints.mediaGenres({ mediaType })
     ),
-    mediaCredits: async ({ mediaType }) => await axiosClient.get(
-        tmdbEndpoints.mediaCredits({ mediaType }),
+    mediaCredits: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaCredits({ mediaType, mediaId })
     ),
     mediaVideos: async ({ mediaType, mediaId }) => await axiosClient.get(
-        tmdbEndpoints.mediaVideos({ mediaType, mediaId }),
+        tmdbEndpoints.mediaVideos({ mediaType, mediaId })
     ),
     mediaImages: async ({ mediaType, mediaId }) => await axiosClient.get(
-        tmdbEndpoints.mediaImages({ mediaType, mediaId }),
+        tmdbEndpoints.mediaImages({ mediaType, mediaId })
     ),
     mediaRecommend: async ({ mediaType, mediaId }) => await axiosClient.get(
-        tmdbEndpoints.mediaRecommend({ mediaType, mediaId }),
+        tmdbEndpoints.mediaRecommend({ mediaType, mediaId })
     ),
     mediaSearch: async ({ mediaType, query, page }) => await axiosClient.get(
-        tmdbEndpoints.mediaSearch({ mediaType, query, page }),
+        tmdbEndpoints.mediaSearch({ mediaType, query, page })
     ),
     personDetail: async ({ personId }) => await axiosClient.get(
-        tmdbEndpoints.personalDetail({ personId })
+        tmdbEndpoints.personDetail({ personId })
     ),
     personMedias: async ({ personId }) => await axiosClient.get(
-        tmdbEndpoints.personalMedias({ personId })
-    ),
+        tmdbEndpoints.personMedias({ personId })
+    )
 };
 
 export default tmdbApi;
